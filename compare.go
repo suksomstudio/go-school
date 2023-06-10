9f4d7aad76ddb46cfaa5bb3403a5b941848e9666
suksomstudio <offroad98@gmail.com>
2023-06-10 18:45:53 +0700

    add file compare.go แสดงการดำเนินการเปรียบเทียบ

---
+++ compare.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	number1, number2 := 10, 3
+
+	fmt.Println("เท่ากันหรือไม่ = ", number1 == number2)
+	fmt.Println("ไม่เท่ากันหรือไม่ = ", number1 != number2)
+	fmt.Println(number1, " มากกว่า ", number2, " = ", number1 > number2)
+	fmt.Println(number1, " น้อยกว่า ", number2, " = ", number1 < number2)
+	fmt.Println(number1, " มากกว่าเท่ากับ ", number2, " = ", number1 >= number2)
+	fmt.Println(number1, " น้อยกว่าเท่ากับ ", number2, " = ", number1 <= number2)
+}
