1d0974488f6add691e00d6e9e25ef00cef78e7eb
suksomstudio <offroad98@gmail.com>
2023-06-10 19:04:35 +0700

    add file scanf.go รับค่าจากคีย์บอร์ด

--- compare.go
+++ compare.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	number1, number2 := 10, 3
-
+	// ตัวดำเนินการเปรียบเทียบข้อมูล
 	fmt.Println("เท่ากันหรือไม่ = ", number1 == number2)
 	fmt.Println("ไม่เท่ากันหรือไม่ = ", number1 != number2)
 	fmt.Println(number1, " มากกว่า ", number2, " = ", number1 > number2)
---
+++ scanf.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// scanf การรับค่าจากคีย์บอร์ด
+func main() {
+	var name string
+	var score int
+	fmt.Println("ป้อนชื่อนักเรียน = ")
+	fmt.Scanf("%s", &name)
+
+	fmt.Println("สวัสดี =", name)
+
+}
