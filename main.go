87975f920f8a409f1532eec2a99894826bf69de9
suksomstudio <offroad98@gmail.com>
2023-06-10 16:45:52 +0700

    ประกาศตัวแปร & add comment

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import "fmt" //นำเอา package เข้ามาทำงาน
+/*
+ให้ทำงานในฟังก์ชั่น main
+มีการแสดงผลออกทางจอภาพ
+โดยการแสดงผลจะอาศัยคำสั่ง fmt.println
+*/
 
 func main() {
-	fmt.Println("Hello Suksom studio")
+	//การนิยามตัวแปรแบบ Manual Type Declaration
+	var name string = "Suksom"
+	var age int = 25
+	var score float64 = 95.8
+	var isPass bool = true
+
 	fmt.Println("Hello Go Programming")
+	fmt.Println("Welcome to Suksom studio")
+
+	fmt.Println("My Name is ", name)
+	fmt.Println("Age ", age)
+	fmt.Println("Score ", score)
+	fmt.Println("Pass Exam ", isPass)
+
+	//การนิยามตัวแปรแบบ Type Inference
+	name1 := "Studio"
+	age1 := 21
+	score1 := 96.8
+	isPass1 := true
+
+	fmt.Println("My Name is ", name1)
+	fmt.Println("Age ", age1)
+	fmt.Println("Score ", score1)
+	fmt.Println("Pass Exam ", isPass1)
 }
